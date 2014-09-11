fdae5eca409e8ca3c2a74995f1dd59968b2078dc
VIXUOWIS <vixuowis@hotmail.com>
2014-09-11 22:52:18 +0800

    Add user controller

--- algso-rails/config/routes.rb
+++ algso-rails/config/routes.rb
@@ -1,10 +1,13 @@
 AlgsoRails::Application.routes.draw do
+
   # get "home/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+  match '/login' ,to: 'user#login' ,via:'get'
+  match '/info' ,to: 'user#info' ,via:'get'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
