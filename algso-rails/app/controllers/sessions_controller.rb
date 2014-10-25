369231302f90af0fcce8b0bffe497eaa08f218d7
vixuowis <yinghuix@qq.com>
2014-10-25 17:41:23 +0800

    Refine code

--- algso-rails/app/controllers/sessions_controller.rb
+++ algso-rails/app/controllers/sessions_controller.rb
@@ -6,9 +6,9 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		@user = User.find_by(email: params[:session][:email].downcase)
-		if @user && @user.authenticate(params[:session][:password])
-			sign_in @user
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			sign_in user
 			redirect_to info_path
 		else
 			flash.now[:error] = '邮箱 / 密码有误'
